dfa8d14ddfffd0f09ab7f89c9283f5d4c44e8019
Mona Lisa <daint@gmail.com>
2022-03-29 13:42:27 +0700

    f

---
+++ DataAccess/Models/WorkflowDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public abstract class WorkflowDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        protected WorkflowDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+    }
+}
