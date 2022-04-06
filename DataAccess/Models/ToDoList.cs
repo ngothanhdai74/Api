c1d6a429c597999dcc6999ae288c13072e8e718d
Mona Lisa <daint@gmail.com>
2022-04-06 09:59:15 +0700

    a

--- DataAccess/Models/ToDoList.cs
+++ DataAccess/Models/ToDoList.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess.Models
 {
+    [Table(nameof(ToDoList))]
     public class ToDoList
     {
+        [Key]
         public long Id { get; set; }
+        [StringLength(255)]
         public string Name { get; set; }
     }
 }
--- DataAccess/Models/ToDoListProps.cs
+++ DataAccess/Models/ToDoListProps.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess.Models
 {
+    [Table(nameof(ToDoListProps))]
     public class ToDoListProps
     {
+        [Key]
         public long Id { get; set; }
+        [StringLength(255)]
         public string Code { get; set; }
+        [StringLength(255)]
         public string Value { get; set; }
     }
 }
