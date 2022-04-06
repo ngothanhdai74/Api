c71ae673041ade13e3e366e0fe13951a60c5920d
Mona Lisa <daint@gmail.com>
2022-04-06 09:35:23 +0700

    A

--- DataAccess/Databases/WorkflowDbContext.cs
+++ DataAccess/Databases/WorkflowDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
@@ -11,10 +12,13 @@ namespace DataAccess.Databases
     public abstract class WorkflowDbContext : DbContext
     {
         protected readonly IConfiguration Configuration;
+        #pragma warning disable CS8618
         protected WorkflowDbContext(IConfiguration configuration)
+        #pragma warning restore CS8618
         {
             Configuration = configuration;
         }
-
+        public DbSet<ToDoList> ToDoLists { get; set; }
+        public DbSet<ToDoListProps> ToDoListProps { get; set; }
     }
 }
