8008d85f7b146e54f85fdd49c521466222349e70
Mona Lisa <daint@gmail.com>
2022-04-06 10:10:11 +0700

    a

---
+++ DataAccess/Data/Migrations/Oracle/20220406031002_CreateDatabase.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Data.Migrations.Oracle
+{
+    public partial class CreateDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDoList",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "NUMBER(19)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Name = table.Column<string>(type: "NVARCHAR2(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoList", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ToDoListProps",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "NUMBER(19)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Code = table.Column<string>(type: "NVARCHAR2(255)", maxLength: 255, nullable: false),
+                    Value = table.Column<string>(type: "NVARCHAR2(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoListProps", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDoList");
+
+            migrationBuilder.DropTable(
+                name: "ToDoListProps");
+        }
+    }
+}
