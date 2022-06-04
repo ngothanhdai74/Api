d82c27972ffbbc66c50bec90ba6183f47f071cae
ngo dai <ngothanhdai74@gmail.com>
2022-06-04 12:27:29 +0700

    s

---
+++ FinanceService/Controllers/CommonController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CommonController : ControllerBase
+    {
+        private readonly ILogger<CommonController> _logger;
+
+        public CommonController(ILogger<CommonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
