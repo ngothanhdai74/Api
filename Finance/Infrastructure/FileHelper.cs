2bd1f704d09b4d876435157fb13cd34888a2f2e1
ngo dai <ngothanhdai74@gmail.com>
2022-06-06 01:05:44 +0700

    s

--- Finance/Infrastructure/FileHelper.cs
+++ Finance/Infrastructure/FileHelper.cs
@@ -1,9 +1,11 @@
 ﻿using System.IO.Compression;
+using System.Text.Json;
 
 namespace Finance.Infrastructure
 {
     public static class FileHelper
     {
+        public static string StartFolderConfig = "LocationRoot";
         public static IEnumerable<FileSystemInfo> AllFilesAndFolders(this DirectoryInfo dir)
         {
             foreach (var f in dir.GetFiles())
@@ -22,17 +24,80 @@ namespace Finance.Infrastructure
         {
             var fileZipResult = Guid.NewGuid().ToString() + ".zip";
             ZipFile.CreateFromDirectory(folderForZip, fileZipResult);
-            MemoryStream ms = new MemoryStream();
             using (FileStream fs = File.OpenRead(fileZipResult))
             {
-                fs.CopyTo(ms);
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    fs.CopyTo(memory);
+                    File.Delete(fileZipResult);
+                    return memory;
+
+                }
             }
-            File.Delete(fileZipResult);
-            return ms;
         }
         public static void UnZip(string fileZipResult, string extractPath)
         {
             ZipFile.ExtractToDirectory(fileZipResult, extractPath);
+        }
+        public static string CreateFolderIfNotExist(
+            this IConfiguration configuration,
+            params string[] folders
+            )
+        {
+            string folderPath = configuration.GetValue<string>(StartFolderConfig);
+            foreach (var folder in folders)
+            {
+                folderPath = Path.Combine(folderPath, folder);
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+            }
+            return folderPath;
+        }
+        public static void SaveFile<T>(
+            this IConfiguration configuration,
+            string fileName,
+            T content,
+            params string[] folders) => File.WriteAllText(Path.Combine(configuration.CreateFolderIfNotExist(folders), fileName), JsonSerializer.Serialize(content));
+        public static T GetFile<T>(
+            this IConfiguration configuration,
+            string filePath
+        ) => JsonSerializer.Deserialize<T>(File.ReadAllText(Path.Combine(configuration.GetValue<string>(StartFolderConfig), filePath)));
+
+
+        public static void CopyFile(
+            this IConfiguration configuration,
+            string sourceFilePath,
+            string destinationFilePath
+            )
+        {
+            string startFolder = configuration.GetValue<string>(StartFolderConfig);
+            string sourceFile = Path.Combine(startFolder, sourceFilePath);
+            string destFile = Path.Combine(startFolder, destinationFilePath);
+        }
+        public static void MoveFile(
+            this IConfiguration configuration
+            )
+        {
+            string startFolder = configuration.GetValue<string>(StartFolderConfig);
+        }
+        public static void DeleteFile(
+            this IConfiguration configuration
+            )
+        {
+            string startFolder = configuration.GetValue<string>("LocationRoot");
+
+            DirectoryInfo dir = new DirectoryInfo(startFolder);
+
+            IEnumerable<FileInfo> fileList = dir.GetFiles("*.*", SearchOption.AllDirectories);
+
+            IEnumerable<System.IO.FileInfo> fileQuery =
+            from file in fileList
+            where file.Extension == ".txt"
+            orderby file.Name
+            select file;
+
         }
     }
 }
