9eeda0c69bfc7ca8fed9db0ff307d2898cc4cb0d
ngo dai <ngothanhdai74@gmail.com>
2022-06-05 10:31:29 +0700

    a

---
+++ Finance/Controllers/ETFController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Finance.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ETFController : ControllerBase
+    {
+        // GET: api/<ETFController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ETFController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ETFController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ETFController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ETFController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
