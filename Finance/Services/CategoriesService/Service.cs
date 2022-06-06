afbf60621d763a214f67dd3c48d75cd51bdaf721
Mona Lisa <daint@gmail.com>
2022-06-06 08:39:12 +0700

    a

--- Finance/Services/CategoriesService/Service.cs
+++ Finance/Services/CategoriesService/Service.cs
@@ -1,6 +1,12 @@
 ﻿namespace Finance.Services.CategoriesService
 {
-    public class Service
+    public class Service : IService
     {
+        private readonly IConfiguration _configuration;
+        private const string BaseFolder = "Categories";
+        public Service(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
     }
 }
