af85662266b0d578ea1e92799209446696a0ef6a
Mona Lisa <daint@gmail.com>
2022-06-06 09:45:06 +0700

    a

--- Finance/Models/BizModels/Categories/New.cs
+++ Finance/Models/BizModels/Categories/New.cs
@@ -2,5 +2,12 @@
 {
     public class New
     {
+        public string Code { get; set; }
+        public string ParentCode { get; set; }
+        public string Name { get; set; }
+        public long Type { get; set; }
+        public string TypeName { get; set; }
+        public long Status { get; set; }
+        public string StatusName { get; set; }
     }
 }
--- Finance/Program.cs
+++ Finance/Program.cs
@@ -5,6 +5,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDependencyInjection();
+builder.Services.AddAutoMapper(typeof(Program));
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
