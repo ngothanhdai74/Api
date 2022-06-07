b14277a8a17da18b64968b36bdbefd567dea3540
ngo dai <ngothanhdai74@gmail.com>
2022-06-07 23:22:57 +0700

    a

--- Finance/Program.cs
+++ Finance/Program.cs
@@ -3,7 +3,10 @@ using Finance.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.CustomSchemaIds(type => type.ToString());
+});
 builder.Services.AddDependencyInjection();
 builder.Services.AddAutoMapper(typeof(Program));
 var app = builder.Build();
