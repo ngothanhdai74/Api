ff301ae2c1cb7bf33bdc088f9a23dc45c96334b4
ngo dai <ngothanhdai74@gmail.com>
2022-06-16 00:57:05 +0700

    h

---
+++ Finance/Models/BaseModels/News.cs
@@ -0,0 +1,15 @@
+﻿namespace Finance.Models.BaseModels
+{
+    public class News
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string KeyWord { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public long Type { get; set; }
+        public string TypeName { get; set; }
+        public long Status { get; set; }
+        public string StatusName { get; set; }
+    }
+}
